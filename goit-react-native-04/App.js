bab3ce7b6cb97a616342dcf46fdcd660e6024341
Bogdan0109 <102528317+Bogdan0109@users.noreply.github.com>
2023-01-04 01:26:38 +0200

    hw-4

---
+++ goit-react-native-04/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import LoginScreen from "./Screens/auth/LoginScreen";
+import RegistrationScreen from "./Screens/auth/RegistrationScreen";
+import Home from "./Screens/auth/Home";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const MainStack = createStackNavigator(); // указывает на группу навигаторов
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <MainStack.Navigator initialRouteName="Login">
+        {/* Замена Switch */}
+        <MainStack.Screen
+          options={{ headerShown: false }}
+          name="Registration"
+          component={RegistrationScreen}
+        />
+        {/* Замена Route */}
+        <MainStack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <MainStack.Screen
+          options={{ headerShown: false }}
+          name="Home"
+          component={Home}
+        />
+      </MainStack.Navigator>
+    </NavigationContainer>
+  );
+};
+// export default function App() {
+//   return <RegistrationScreen />;
+// }
